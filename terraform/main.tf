08afc48a7b26581860a2845111564230a137711d
Jinka <hari.prasad.jinka@accenture.com>
2024-12-10 23:16:32 +0530

    added account sandbox-aft3

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "hariaws009+sandboxtwo@gmail.com"
-    AccountName               = "sandbox-aft2"
+    AccountEmail              = "hariaws009+sandboxthree@gmail.com"
+    AccountName               = "sandbox-aft3"
     ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "hariaws009+sandboxtwo@gmail.com"
+    SSOUserEmail              = "hariaws009+sandboxthree@gmail.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
